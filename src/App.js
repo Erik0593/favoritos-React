6c93f1eed3ef5621419188f55f08bf5a3ef7338d
Erik Gutierrez <gutierrez_m_e@hotmail.com>
2022-03-17 18:55:39 -0600

    clase jueves17

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 // import api from '../src/lib/api'
-import {
-  Card, CardBody, CardTitle, CardSubtitle, CardText, Button, CardImg, Form, FormGroup, Input, Label,
-  FormText, Container, Col, Row
-} from 'reactstrap'
+// import {
+//   Card, CardBody, CardTitle, CardSubtitle, CardText, Button, CardImg, Form, FormGroup, Input, Label,
+//   FormText, Container, Col, Row
+// } from 'reactstrap'
 
 function App() {
   const BASE_URL = 'https://crud-vacantes-default-rtdb.firebaseio.com'
-  const [listJob, setListJob] = useState({})
-  const [listApplied, setListApplied] = useState({})
-  const [jobData, setJobData] = useState({})
-
+  
+  const [listJob , setListJob] = useState({})
 
 
   useEffect(() => {
@@ -24,202 +22,25 @@ function App() {
   }, [])
 
 
-
-  const buttonApply = event => {
-    let property = event.target.name
-    let value = event.target.value
-    setListApplied = { ...listApplied , [property]:value}
-    console.log(property, value)
-  }
-
-  const formHandler = event => {
-    let property = event.target.name
-    let value = event.target.value
-    setJobData({ ...jobData, [property]: value })
-    console.log(jobData)
-  }
-
-  const buttonCreate = async () => {
-    setListJob({ ...listJob }, jobData)
-    let response = await fetch(`${BASE_URL}/vacante.json`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(jobData)
-    })
-    let response2 = await response.json()
-    console.log(response2)
-    if (response2.name) {
-      fetch(`${BASE_URL}/vacante.json`).then(response => {
-        return response.json()
-      })
-        .then((data) => {
-          setListJob(data)
-        })
-    }
-    return response2
-  }
-
-  const buttonDelete = async (event) => {
-    let jobId = event.target.dataset.jobId
-    console.log(jobId)
-    let response = await fetch(`${BASE_URL}/vacante/${jobId}.json`, {
-      method: 'DELETE'
-    })
-  }
-
-
-
-
   return (
     <div className="App">
-
-      <div>
-        <Container>
-          <Row>
-            <Col md="6">
-            <div>
-              {
-                Object.keys(listJob).map(job => {
-                  const { img, vacante, tipo, tags } = listJob[job]
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-6">
+              {Object.keys(listJob).map( job => {
                   return (
-                    <Card key={job}
-                      jobData={{ ...listJob[job] }}
-                    className="card text-white bg-dark mb-3">
-                      <CardBody>
-                        <CardImg
-                          alt="Card image cap"
-                          src={img}
-                        />
-                        <CardTitle tag="h5">
-                          {vacante}
-                        </CardTitle>
-                        <CardSubtitle
-                          className="mb-2 text-muted"
-                          tag="h6"
-                        >
-                          {tipo}
-                        </CardSubtitle>
-                        <CardText>
-                          {tags}
-                        </CardText>
-                        <Button className='btn btn-success' onClick={buttonApply}>
-                          Aplicar
-                        </Button>
-                        <Button className='btn btn-danger' onClick={buttonDelete}>
-                          Eliminar
-                        </Button>
-
-                      </CardBody>
-                    </Card>
+                    <Card 
+                    titulo={titulo}
+                    imagen={img}
+                    tipo={tipo}
+                    tag={tags}
+                    />
                   )
-                })
+              })
               }
             </div>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col md="6">
-            <Form>
-              <FormGroup>
-                <Label for="exampleText"> Nombre de la Vacante</Label>
-                <Input
-                  id="exampleText"
-                  name="vacante"
-                  type="text"
-                  onChange={formHandler}
-                />
-              </FormGroup>
-
-
-              <FormGroup>
-                <Label for="exampleSelect">
-                  Select
-                </Label>
-                <Input
-                  id="exampleSelect"
-                  name="tipo"
-                  type="select"
-                  onChange={formHandler}
-                >
-                  <option>
-                    Full-time
-                  </option>
-                  <option>
-                    Part-time
-                  </option>
-                  <option>
-                    Remote
-                  </option>
-                </Input>
-              </FormGroup>
-
-
-              <FormGroup>
-                <Label for="exampleFile">
-                  Imagen Empresa
-                </Label>
-                <Input
-                  id="exampleFile"
-                  name="img"
-                  type="text"
-                  onChange={formHandler}
-                />
-              </FormGroup>
-
-              <FormGroup tag="fieldset">
-                <legend>
-                  Skills
-                </legend>
-                <FormGroup check>
-                  <Input
-                    name="javaScript"
-                    type="checkbox"
-                    onChange={formHandler}
-                  />
-                  {' '}
-                  <Label check>
-                    JavaScript
-                  </Label>
-                </FormGroup>
-                <FormGroup check>
-                  <Input
-                    name="CSS"
-                    type="checkbox"
-                    onChange={formHandler}
-                  />
-                  {' '}
-                  <Label check>
-                    CSS
-                  </Label>
-                </FormGroup>
-                <FormGroup
-                  check
-                  disabled
-                >
-                  <Input
-                    name="React"
-                    type="checkbox"
-                    onChange={formHandler}
-                  />
-                  {' '}
-                  <Label check>
-                    React
-                  </Label>
-                </FormGroup>
-              </FormGroup>
-              <Button onClick={buttonCreate}>
-                Crear Vacante
-              </Button>
-            </Form>
-            </Col>
-          </Row>
-        </Container>
-
-      </div>
-
+          </div>
+        </div>
     </div>
   );
 }
